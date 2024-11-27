eeab1a63f7bc47bd0846dd852bf1faeb4f880d9c
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-11-27 23:53:03 +0300

    static files added

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,186 @@
+'use strict';
+
+const usernamePage = document.querySelector('#username-page');
+const chatPage = document.querySelector('#chat-page');
+const usernameForm = document.querySelector('#username-form');
+const messageForm = document.querySelector('#message-form');
+const messageInput = document.querySelector('#message-input');
+const connectingElement = document.querySelector('.connecting');
+const chatArea = document.querySelector('#chat-messages');
+const logoutButton = document.querySelector('#logout');
+
+let stompClient = null;
+let username = null;
+let fullName = null;
+let selectedUserId = null;
+
+function connect(event) {
+    username = document.querySelector('#username-input').value.trim();
+    fullName = document.querySelector('#full-name-input').value.trim();
+
+    if (username && fullName) {
+        usernamePage.classList.add('hidden');
+        chatPage.classList.remove('hidden');
+
+        const socket = new SockJS('/ws');
+        stompClient = Stomp.over(socket);
+
+        stompClient.connect({}, onConnected, onError);
+    }
+    event.preventDefault();
+}
+
+function onConnected() {
+    stompClient.subscribe(`/user/${username}/queue/messages`, onMessageReceived);
+    stompClient.subscribe(`/user/public`, onMessageReceived);
+
+    // Register the connected user
+    stompClient.send(
+        '/app/user.addUser',
+        {},
+        JSON.stringify({ username: username, fullName: fullName, status: 'ONLINE' })
+    );
+    document.querySelector('#connected-user-fullname').textContent = fullName;
+    findAndDisplayConnectedUsers().then();
+}
+
+async function findAndDisplayConnectedUsers() {
+    const response = await fetch('/users');
+    let connectedUsers = await response.json();
+    connectedUsers = connectedUsers.filter(user => user.username !== username);
+    const connectedUsersList = document.getElementById('connected-users');
+    connectedUsersList.innerHTML = '';
+
+    connectedUsers.forEach(user => {
+        appendUserElement(user, connectedUsersList);
+        if (connectedUsers.indexOf(user) < connectedUsers.length - 1) {
+            const separator = document.createElement('li');
+            separator.classList.add('separator');
+            connectedUsersList.appendChild(separator);
+        }
+    });
+}
+
+function appendUserElement(user, connectedUsersList) {
+    const listItem = document.createElement('li');
+    listItem.classList.add('user-item');
+    listItem.id = user.username;
+
+    const userImage = document.createElement('img');
+    userImage.src = '../img/user_icon.png';
+    userImage.alt = user.fullName;
+
+    const usernameSpan = document.createElement('span');
+    usernameSpan.textContent = user.fullName;
+
+    const unreadMessages = document.createElement('span');
+    unreadMessages.textContent = '0';
+    unreadMessages.classList.add('unread-messages', 'hidden');
+
+    listItem.appendChild(userImage);
+    listItem.appendChild(usernameSpan);
+    listItem.appendChild(unreadMessages);
+
+    listItem.addEventListener('click', onUserItemClick);
+
+    connectedUsersList.appendChild(listItem);
+}
+
+function onUserItemClick(event) {
+    document.querySelectorAll('.user-item').forEach(item => {
+        item.classList.remove('active');
+    });
+    messageForm.classList.remove('hidden');
+
+    const clickedUser = event.currentTarget;
+    clickedUser.classList.add('active');
+
+    selectedUserId = clickedUser.getAttribute('id');
+    fetchAndDisplayUserChat().then();
+
+    const unreadMessages = clickedUser.querySelector('.unread-messages');
+    unreadMessages.classList.add('hidden');
+    unreadMessages.textContent = '0';
+}
+
+function displayMessage(senderId, content) {
+    const messageContainer = document.createElement('div');
+    messageContainer.classList.add('message');
+    if (senderId === username) {
+        messageContainer.classList.add('sender');
+    } else {
+        messageContainer.classList.add('receiver');
+    }
+    const message = document.createElement('p');
+    message.textContent = content;
+    messageContainer.appendChild(message);
+    chatArea.appendChild(messageContainer);
+}
+
+async function fetchAndDisplayUserChat() {
+    const response = await fetch(`/messages/${username}/${selectedUserId}`);
+    const userChat = await response.json();
+    chatArea.innerHTML = '';
+    userChat.forEach(chat => {
+        displayMessage(chat.senderId, chat.content);
+    });
+    chatArea.scrollTop = chatArea.scrollHeight;
+}
+
+function onError() {
+    connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
+    connectingElement.style.color = 'red';
+}
+
+function sendMessage(event) {
+    const messageContent = messageInput.value.trim();
+    if (messageContent && stompClient) {
+        const chatMessage = {
+            senderId: username,
+            recipientId: selectedUserId,
+            content: messageContent,
+            timestamp: new Date(),
+        };
+        stompClient.send('/app/chat', {}, JSON.stringify(chatMessage));
+        displayMessage(username, messageContent);
+        messageInput.value = '';
+    }
+    chatArea.scrollTop = chatArea.scrollHeight;
+    event.preventDefault();
+}
+
+async function onMessageReceived(payload) {
+    await findAndDisplayConnectedUsers();
+    const message = JSON.parse(payload.body);
+    if (selectedUserId && selectedUserId === message.senderId) {
+        displayMessage(message.senderId, message.content);
+        chatArea.scrollTop = chatArea.scrollHeight;
+    }
+
+    if (selectedUserId) {
+        document.querySelector(`#${selectedUserId}`).classList.add('active');
+    } else {
+        messageForm.classList.add('hidden');
+    }
+
+    const notifiedUser = document.querySelector(`#${message.senderId}`);
+    if (notifiedUser && !notifiedUser.classList.contains('active')) {
+        const unreadMessages = notifiedUser.querySelector('.unread-messages');
+        unreadMessages.classList.remove('hidden');
+        unreadMessages.textContent = '';
+    }
+}
+
+function onLogout() {
+    stompClient.send(
+        '/app/user.disconnectUser',
+        {},
+        JSON.stringify({ username: username, fullName: fullName, status: 'OFFLINE' })
+    );
+    window.location.reload();
+}
+
+usernameForm.addEventListener('submit', connect, true);
+messageForm.addEventListener('submit', sendMessage, true);
+logoutButton.addEventListener('click', onLogout, true);
+window.onbeforeunload = () => onLogout();
